f7e1c49edd0de85eb5c0a66d82e5430f38697c43
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2021-11-14 14:28:01 -0500

    Sum output almost operational TODO: iterate through each player instead of summing all players

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -7,47 +7,52 @@ import java.util.Scanner;
 public class
 Main {
     private static int TOTAL_ROUNDS = 10;
-    private static int TOTAL_GAMES = 3;
+    private static int TOTAL_PLAYERS = 3;
     private static int score = 0;
+    private static int total = 0;
     private static int sum = 0;
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         String[] names = {"Angaran", "Taylor", "Tom"};
-        //String[] players = new String[3];
-        //int[][] playerScores = new int[3][TOTAL_ROUNDS];
-       /* for (int i = 0; i < names.length; i++) {
-            System.out.println("Please enter the score for "+ names[i] + game[]);
-        }*/
+        String[] player = new String[3];
+        int[][] playerScores = new int[TOTAL_PLAYERS][TOTAL_ROUNDS];
+
         boolean isTrue = false;
         while (!isTrue) {
             /* Loop through each player, for 10 rounds each */
-            for (int i = 0; i <= TOTAL_GAMES; i++)
-            {
-                for (int count = 1; count <= TOTAL_ROUNDS ; count++) {
+            for (int p = 0; p <= TOTAL_PLAYERS; p++) {
+                for (int count = 1; count <= TOTAL_ROUNDS; count++) {
 
                     try {
-                        //System.out.print("Please enter the score for" + players[count] + " : ");
-                        System.out.print("Please enter the score for " + names[i] + " in round" + count);
+                        // Error output
+                        System.out.printf("Please enter the score for " + names[p] + " in round" + count);
                         score = input.nextInt();
 
                         isTrue = true;
 
                         if (score <= 0 || score > 300) {
+                            // Error output
                             System.out.println("The score must be between 0 and 300");
                             input.nextLine();
                             isTrue = false;
+
                         } else {
 
-                            score++;
-                            sum += score; //TODO: Display sum of each players score to be displayed after inputting 10 scores for player 1, player 2 and player 3
+                            // add score to sum //TODO: iterate through each player instead of suming all players
+                                sum += score;
+
+                            // Output of score sum
+                            System.out.println("Sum is : " + sum);
+
 
                         }
 
+
                     } catch (InputMismatchException e) {
+                        // Error output
                         System.out.println("You must enter a valid integer");
                         input.nextLine();
                         isTrue = false;
@@ -58,5 +63,4 @@ Main {
         }
 
     }
-
 }
