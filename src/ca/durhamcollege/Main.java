70db3cb9bac5b3da7bc92d060da46f13a4fee84e
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2021-11-14 17:21:48 -0500

    updated to include average

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,12 +1,19 @@
 package ca.durhamcollege;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
+/*
+*  @author Angaran Yogeswaran XXXXXXXXX
+*  @author Taylor Bazouzi 100579090
+*  @lastModified November 14, 2021
+*  @Description Java console program that accepts user inputted scores for three players
+*               and then displays the total of each players scores
+*
+* */
 
 
 public class
 Main {
-    private static int TOTAL_ROUNDS = 10;
+    private static int TOTAL_ROUNDS = 2;
     private static int TOTAL_PLAYERS = 3;
     private static int score = 0;
     private static int total = 0;
@@ -17,15 +24,17 @@ Main {
         Scanner input = new Scanner(System.in);
 
         String[] names = {"Angaran", "Taylor", "Tom"};
-        String[] player = new String[3];
+
+        /* String[] player = new String[3]; */
+
         int[][] playerScores = new int[TOTAL_PLAYERS][TOTAL_ROUNDS];
 
         boolean isTrue = false;
         while (!isTrue) {
             /* Loop through each player, for 10 rounds each */
-            for (int p = 0; p <= TOTAL_PLAYERS; p++)
+            for (int p = 0 ; p <= TOTAL_PLAYERS; p++)
             {
-                for (int count = 1; count <= TOTAL_ROUNDS; count++)
+                for (int count = 0+1 ; count <= TOTAL_ROUNDS; count++)
                 {
                     try
                     {
@@ -33,24 +42,18 @@ Main {
                         System.out.printf("Please enter the score for " + names[p] + " in round" + count);
                         score = input.nextInt();
 
-
                         isTrue = true;
 
                         if (score <= 0 || score > 300)
                         {
                             // Error output
                             System.out.println("The score must be between 0 and 300");
-                            input.nextLine();
+                            score = input.nextInt();
                             isTrue = false;
 
                         } else {
 
-
-                            // add score to sum //TODO: iterate through each player instead of suming all players
                              sum += score;
-
-                            // Output of score sum
-                            //System.out.println("Sum is : " + sum);
                         }
                     } catch (InputMismatchException e) {
                         // Error output
@@ -58,13 +61,12 @@ Main {
                         input.nextLine();
                         isTrue = false;
                     }
-
                 }
-                // Output of score sum
-                System.out.println("Sum is : " + sum);
-
+                // Output of score average
+                int average = sum / TOTAL_ROUNDS;
+                System.out.println("Average Score: "+ average);
+                //System.out.println("Sum is : " + sum);
             }
         }
-
     }
 }
